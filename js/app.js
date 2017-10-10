c4d0e9e8001f551cf8847586f153b73836be464b
russelu <yizhoulu@umich.edu>
2017-10-10 00:19:49 -0700

    using with bindings

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+
+	this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T']);
 
 	this.level = ko.computed(function() {
 		if (this.clickCount()<10) {
@@ -13,8 +15,16 @@ var ViewModel = function() {
 		}
 	}, this);
 
+}
+
+var ViewModel = function() {
+
+	var self = this;
+
+	this.currentCat = ko.observable(new Cat());
+
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount()+1);
+		self.currentCat().clickCount(self.currentCat().clickCount()+1);
 	};
 }
 
